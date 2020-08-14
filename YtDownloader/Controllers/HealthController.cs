3a2b838f0d597e786e8d7230f9f1cb7fa383d513
Serenity <d.palbanese@gmail.com>
2020-08-14 21:17:07 +0200

    Added health endpoint

---
+++ YtDownloader/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using ArgonautCore.Network.Health.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YtDownloader.Controllers
+{
+    [ApiController]
+    [Route("[controller]/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<HealthStatus> GetHealthStatus()
+        {
+            return Ok(new HealthStatus("Youtube Downloader Service", Status.Healthy)
+            {
+                Description = "Easily convert and download youtube videos to mp3 or mp4, without viruses, ads and the highest bitrate."
+            });
+        }
+    }
+}
